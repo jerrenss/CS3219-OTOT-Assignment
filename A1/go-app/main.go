3f16727c846cc585725baafa00c448bf2d0b907d
jerrenss <gerrenseow@gmail.com>
2021-08-22 05:44:10 +0800

    feat: added implementation of Task A1

---
+++ A1/go-app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "net/http"
+  "github.com/gin-gonic/gin"
+)
+
+func main() {
+  // Gin router initialization
+  router := gin.Default()
+
+  // GET endpoint
+  router.GET("/api", func(context *gin.Context) {
+    context.JSON(http.StatusOK, gin.H{"data": "Welcome to the Go app!"})    
+  })
+
+  // Run application  on port 4000
+  router.Run(":4000") 
+}
