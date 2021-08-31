061f57faa1fd2ae46eeacd7b47a184cbeb3ca8e1
jerrenss <gerrenseow@gmail.com>
2021-08-31 17:58:41 +0800

    feat: added API for Task F

--- F/server/go.mod
+++ F/server/go.mod
@@ -2,4 +2,7 @@ module b2-server
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.4
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-redis/redis v6.15.9+incompatible
+)
--- F/server/main.go
+++ F/server/main.go
@@ -1,19 +1,68 @@
 package main
 
 import (
+  "fmt"
   "net/http"
+  "io/ioutil"
+  "encoding/json"
+  "log"
   "github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 )
+
+type PostResponse struct {
+  UserID       int `json:"userId"`
+  ID    int `json:"id"`
+  Title      int `json:"title"`
+  Body      int `json:"body"`
+}
 
 func main() {
   // Gin router initialization
   router := gin.Default()
 
-  // GET endpoint
-  router.GET("/api", func(context *gin.Context) {
-	  context.JSON(http.StatusOK, gin.H{"data": "Welcome to the Go app!"})    
+  // Connect to Redis
+  redisClient := redis.NewClient(&redis.Options{
+		Addr: "cache:6379",
+		Password: "",
+		DB: 0,
+	})
+
+  _, err := redisClient.Ping().Result()
+  if err == nil {
+    fmt.Println("Server is successfully connected to Redis!")
+  } else {
+    fmt.Println(err)
+  }
+
+  // Home endpoint
+  router.GET("/", func(c *gin.Context) {
+	  c.JSON(http.StatusOK, gin.H{"data": "Welcome to the Go app!"})    
   })
 
-  // Run application  on port 4000
+  // Data endpoint
+  router.GET("/api/posts", func(c *gin.Context) {
+    resp, err := http.Get("http://jsonplaceholder.typicode.com/posts")
+
+    if err != nil {
+      log.Fatal(err)
+      c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    }
+
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var result []PostResponse
+    if err := json.Unmarshal(body, &result); err != nil {  // Parse []byte to the go struct pointer
+        fmt.Println("Can not unmarshal JSON")
+    }
+
+    c.JSON(http.StatusOK, gin.H{"data": result})    
+  })
+
   router.Run(":4000") 
 }
