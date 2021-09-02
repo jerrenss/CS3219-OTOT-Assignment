d44a56cc23da63e0b0724f882e9235cb05070f98
jerrenss <gerrenseow@gmail.com>
2021-09-03 02:03:23 +0800

    fix: update test setup for Task B

--- B/server/test/run-test.sh
+++ B/server/test/run-test.sh
@@ -1,15 +1,18 @@
 # Start PostgreSQL database for test
-docker-compose up -d
+docker-compose -f docker-compose-test.yml up -d
 
 # Ping for readiness
-echo "[test-database] not yet ready to accept connections"
+echo "[db] not yet ready to accept connections"
 WAIT_FOR_PG_ISREADY="while ! pg_isready; do sleep 1; done;"
 docker-compose exec test-database bash -c "$WAIT_FOR_PG_ISREADY"
-echo "[test-database] ready to accept connections"
+echo "[db] ready to accept connections"
+
+# Temporary fix to ensure container is fully ready
+sleep 5
 
 # Execute test suite
 echo "running all tests..."
-yarn test
+../node_modules/mocha/bin/mocha '../test/**/*-test.js' --recursive --timeout 10000 --exit
 
 # Clean up
-docker-compose down -v
+docker-compose -f docker-compose-test.yml down -v
