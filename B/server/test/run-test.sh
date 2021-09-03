ab11b7b67a5e09ff062ae136904ba6f78dffdb95
jerrenss <gerrenseow@gmail.com>
2021-09-03 14:07:02 +0800

    ci: updated yaml file for Task B

--- B/server/test/run-test.sh
+++ B/server/test/run-test.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 # Start PostgreSQL database for test
 docker-compose -f docker-compose-test.yml up -d
 
 # Ping for readiness
-until psql "postgres://OTOT-B-USER:OTOT-B-PW@localhost:5434/OTOT-B" -c "select 1" > /dev/null 2>&1 || [ 10 -eq 0 ]; do
+RETRIES=10
+until psql "postgres://OTOT-B-USER:OTOT-B-PW@localhost:5434/OTOT-B" -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 1
 done
